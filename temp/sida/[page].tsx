15c82d04a92717866aeb1d0c2b7f5046c431c529
Bebe Jane <bjornthief@gmail.com>
2024-09-06 17:02:01 +0200

    clean

--- temp/sida/[page].tsx
+++
@@ -1,7 +0,0 @@
-import { getStaticPaginationPaths } from '/lib/utils'
-import { AllKnowledgesDocument } from "/graphql";
-export { default, getStaticProps } from '..'
-
-export const getStaticPaths = () => {
-  return getStaticPaginationPaths(AllKnowledgesDocument, 'knowledges')
-}
