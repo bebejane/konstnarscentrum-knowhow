1b2fc6e59391663cc881408d2c1fc0207bf6d4c0
Bebe Jane <bjornthief@gmail.com>
2024-10-22 11:48:11 +0200

    hallo

---
+++ test.ts
@@ -0,0 +1,17 @@
+import dotenv from 'dotenv'
+dotenv.config({ path: './.env' })
+import { buildClient } from '@datocms/cma-client'
+const client = buildClient({
+  apiToken: process.env.DATOCMS_API_TOKEN,
+  environment: process.env.DATOCMS_ENVIRONMENT ?? 'main'
+})
+
+const getUserByEmail = async (email: string) => {
+  return (await client.items.list({ page: { limit: 1 }, filter: { type: 'member', fields: { email: { eq: email } } } }))?.[0]
+}
+
+const main = async () => {
+  const user = await getUserByEmail('tobias.sjoedin@gmail.com')
+  console.log(user)
+}
+main()
