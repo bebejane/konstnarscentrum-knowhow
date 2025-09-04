234bdc5b1335486ac2dd417d6cb2daae72dae661
Bebe Jane <bjornthief@gmail.com>
2025-09-04 11:31:10 +0200

    kolla

---
+++ components/blocks/ImageShortcutDouble.tsx
@@ -0,0 +1,37 @@
+import s from './ImageShortcutDouble.module.scss'
+import cn from 'classnames'
+import React from 'react'
+import { KCImage as Image } from '/components'
+import ReadMore from '../common/ReadMore'
+
+export type ImageShortcutDoubleBlockProps = {
+  data: ImageShortcutDoubleRecord
+}
+
+export default function ImageShortcutDouble({ data: {shortcuts } }: ImageShortcutDoubleBlockProps) {
+
+  return (
+    <section className={s.container}>
+      {shortcuts.map(({image, headline, text, link, blackHeadline}, idx) =>
+        <figure key={idx}>
+          {image &&
+            <Image
+              className={s.image}
+              data={image.responsiveImage}
+              objectFit={'cover'}
+              intersectionMargin="0px 0px 200% 0px"
+            />
+          }
+          <figcaption>
+            <div className={s.fade}></div>
+            <h2 className={cn(blackHeadline && s.black)}>
+              {headline}
+            </h2>
+            <p className={cn(blackHeadline && s.black, "intro")}>{text}</p><br />
+            <ReadMore link={link} message={link === '/english' ? 'Read more' : 'Läs mer'} external={true} regional={false} invert={blackHeadline ? false : true} />
+          </figcaption>
+        </figure>
+      )}
+    </section >
+  )
+}
