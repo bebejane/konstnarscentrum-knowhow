cff79b3c22dd0bbe4d7ad71197f747a398dec8c6
Bebe Jane <bjornthief@gmail.com>
2026-04-23 13:56:19 +0200

    fixar

--- components/common/FilterBarDropdown.tsx
+++ components/common/FilterBarDropdown.tsx
@@ -3,9 +3,11 @@
 import Link from 'next/link';
 import s from './FilterBarDropdown.module.scss';
 import cn from 'classnames';
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { sortSwedish } from 'next-dato-utils/utils';
 import { useOutsideClickRef } from 'rooks';
+import { Modal } from 'next-dato-utils/components';
+import { useWindowSize } from 'usehooks-ts';
 
 type FilterDropdownOption = {
 	key: string;
@@ -23,13 +25,16 @@ type Props = {
 };
 
 export default function FilterBarDropdown({ options = [], params, pathname }: Props) {
-	const [open, setOpen] = useState<Record<string, boolean>>({});
+	const [dropdown, setDrowpdown] = useState<FilterDropdownOption | null>();
+	const [dropdownStyles, setDrowpdownStyles] = useState<React.CSSProperties | null>(null);
+	const [filterScrollPosition, setFilterScrollPosition] = useState<number>(0);
 	const [values, setValues] = useState<
 		Record<string, FilterDropdownOption['items'][number] | null | undefined>
 	>({});
+	const { width, height } = useWindowSize();
 
 	const [ref] = useOutsideClickRef(() => {
-		setOpen({});
+		setDrowpdown(null);
 	});
 
 	function isSelected(id: string) {
@@ -40,16 +45,16 @@ export default function FilterBarDropdown({ options = [], params, pathname }: Pr
 
 	function toggle(e: React.MouseEvent<HTMLSpanElement>) {
 		const key = e.currentTarget.dataset.key as string;
-		setOpen((prev) => ({ [key]: !prev[key] }));
+		const dropdown = options.find((el) => el.key === key) ?? null;
+		setDrowpdown((d) => (d?.key === dropdown?.key ? null : dropdown));
 	}
 
 	function handleClick(e: React.MouseEvent<HTMLSpanElement>) {
-		console.log('click');
 		const key = e.currentTarget.dataset.key as string;
 		const itemId = e.currentTarget.dataset.itemId as string;
 		const item = options.find((el) => el.key === key)?.items.find((el) => el.id === itemId) ?? null;
 		setValues((prev) => ({ ...prev, [key]: item }));
-		setOpen({});
+		setDrowpdown(null);
 	}
 
 	function getQuery(item?: FilterDropdownOption['items'][number]) {
@@ -90,48 +95,72 @@ export default function FilterBarDropdown({ options = [], params, pathname }: Pr
 		};
 	}
 
+	useEffect(() => {
+		if (dropdown) {
+			const el = document.getElementById(dropdown.key);
+
+			if (!el) return setDrowpdownStyles(null);
+			const bounds = el.getBoundingClientRect();
+
+			setDrowpdownStyles({
+				top: `calc(${bounds.top + bounds.height + 1}px + 1em)`,
+				left: bounds.left,
+			});
+		}
+	}, [dropdown, width, height, filterScrollPosition]);
+
 	return (
-		<nav className={s.filter} ref={ref}>
-			<ul>
-				{options.map((opt, idx) => (
-					<li key={idx}>
-						<div className={s.label}>{opt.label}: </div>
-						<div className={s.value}>
-							<button className={cn(open[opt.key] && s.open)} onClick={toggle} data-key={opt.key}>
-								{values[opt.key]?.label ?? 'Alla'}
-							</button>
-							<ul className={cn(s.options, open[opt.key] && s.open)}>
-								{values[opt.key] && (
-									<li>
-										<Link
-											prefetch={true}
-											data-key={opt.key}
-											href={{ pathname, query: getQuery() }}
-											onClick={handleClick}
-										>
-											Alla
-										</Link>
-									</li>
-								)}
-								{sortSwedish(opt.items, 'label').map((item, idx) => (
-									<li key={idx} className={cn(isSelected(item.id) && s.selected)}>
-										<Link
-											prefetch={true}
-											data-key={opt.key}
-											data-item-id={item.id}
-											onClick={handleClick}
-											href={{ pathname, query: getQuery(item) }}
-										>
-											{item.label}
-										</Link>
-									</li>
-								))}
-							</ul>
-						</div>
-					</li>
-				))}
-			</ul>
-			<div className={s.background}></div>
-		</nav>
+		<>
+			<nav className={s.filter} ref={ref}>
+				<ul onScroll={(e) => setFilterScrollPosition(e.currentTarget.scrollLeft)}>
+					{options.map((opt, idx) => (
+						<li key={idx}>
+							<div className={s.label}>{opt.label}: </div>
+							<div id={opt.key} className={s.value}>
+								<button
+									className={cn(dropdown?.key === opt.key && s.open)}
+									onClick={toggle}
+									data-key={opt.key}
+								>
+									{values[opt.key]?.label ?? 'Alla'}
+								</button>
+							</div>
+						</li>
+					))}
+				</ul>
+				<div className={s.background}></div>
+			</nav>
+			{dropdown && (
+				<Modal>
+					<ul className={cn(s.dropdown, dropdown && s.open)} style={dropdownStyles ?? undefined}>
+						{values[dropdown.key] && (
+							<li>
+								<Link
+									prefetch={true}
+									data-key={dropdown.key}
+									href={{ pathname, query: getQuery() }}
+									onClick={handleClick}
+								>
+									Alla
+								</Link>
+							</li>
+						)}
+						{sortSwedish(dropdown.items, 'label').map((item, idx) => (
+							<li key={idx} className={cn(isSelected(item.id) && s.selected)}>
+								<Link
+									prefetch={true}
+									data-key={dropdown.key}
+									data-item-id={item.id}
+									onClick={handleClick}
+									href={{ pathname, query: getQuery(item) }}
+								>
+									{item.label}
+								</Link>
+							</li>
+						))}
+					</ul>
+				</Modal>
+			)}
+		</>
 	);
 }
